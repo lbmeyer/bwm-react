96034fb09a2a9de6b4764257b24566a385fb363e
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-02-19 17:14:16 -0500

    Installed react-google-maps

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchRentalById } from '../../../actions';
 import RentalDetailInfo from './RentalDetailInfo';
+import { MapWithAMarker } from '../../../map/GoogleMap';
 
 class RentalDetail extends Component {
   componentDidMount() {
@@ -17,28 +18,32 @@ class RentalDetail extends Component {
     return (
       <>
         {rental._id ? (
-          <section id='rentalDetails'>
-          <div className='upper-section'>
-            <div className='row'>
-              <div className='col-md-6'>
-                <img src={rental.image} alt=''></img>
-              </div>
-              <div className='col-md-6'>
-                <img src={rental.image} alt=''></img>
+          <section id="rentalDetails">
+            <div className="upper-section">
+              <div className="row">
+                <div className="col-md-6">
+                  <img src={rental.image} alt="" />
+                </div>
+                <div className="col-md-6">
+                  <MapWithAMarker
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDW9tFSqG2mA0ym2NluRBVGZ6tPr8xbwRM&v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        
-          <div className='details-section'>
-            <div className='row'>
-              <div className='col-md-8'>
-                <RentalDetailInfo rental={rental} />
+
+            <div className="details-section">
+              <div className="row">
+                <div className="col-md-8">
+                  <RentalDetailInfo rental={rental} />
+                </div>
+                <div className="col-md-4"> BOOKING</div>
               </div>
-              <div className='col-md-4'> BOOKING</div>
             </div>
-          </div>
-        </section>
-        
+          </section>
         ) : (
           <h1>Loading...</h1>
         )}
