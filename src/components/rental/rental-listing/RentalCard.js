7af45686dd04cfbddc9da36762b2a96256058e51
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-02-19 16:50:54 -0500

    Added helpers file

--- src/components/rental/rental-listing/RentalCard.js
+++ src/components/rental/rental-listing/RentalCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { rentalType } from '../../../helpers/index';
+
 function RentalCard({ colNum, rental }) {
   return (
     <div className={colNum}>
@@ -13,7 +15,7 @@ function RentalCard({ colNum, rental }) {
           />
           <div className="card-block">
             <h6 className={`card-subtitle ${rental.category}`}>
-              {rental.shared ? 'shared' : 'whole'} {rental.category} &#183;{' '}
+              {rentalType(rental.shared)} {rental.category} &#183;{' '}
               {rental.city}
             </h6>
             <h4 className="card-title">{rental.title}</h4>
