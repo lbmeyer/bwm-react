a28010710ab666a430726ac0bc85a2bec5ce2e38
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-02-21 17:57:44 -0500

    Installed bcrypt and added it to userSchema.pre cb function

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -27,5 +28,17 @@ const userSchema = new Schema({
   rentals: [{type: Schema.Types.ObjectId, ref: 'Rental'}],
   // bookings: [{ type: Schema.Types.ObjectId, ref: 'Booking' }]
 });
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      user.password = hash;
+      next(); // call fn that is next in queue --> save 
+    });
+  });
+})
+
 
 module.exports = mongoose.model('User', userSchema);
